60c19eb48a78c7248b1c098005869218cde2d037
Vitaliy <theyamshikov@yandex.ru>
2021-01-16 21:05:20 +0600

    upd

--- bin/bootstrap.php
+++ bin/bootstrap.php
@@ -10,22 +10,6 @@ use ZnLib\Console\Symfony4\Helpers\CommandHelper;
  * @var Container $container
  */
 
-
-/*$container->bind(\ZnTool\Dev\Composer\Domain\Interfaces\Repositories\ConfigRepositoryInterface::class, \ZnTool\Dev\Composer\Domain\Repositories\File\ConfigRepository::class);
-$container->bind(\ZnTool\Dev\Composer\Domain\Interfaces\Services\ConfigServiceInterface::class, \ZnTool\Dev\Composer\Domain\Services\ConfigService::class);
-
-// --- Package ---
-
-$container->bind(\ZnTool\Package\Domain\Interfaces\Services\GitServiceInterface::class, \ZnTool\Package\Domain\Services\GitService::class);
-$container->bind(\ZnTool\Package\Domain\Interfaces\Services\PackageServiceInterface::class, \ZnTool\Package\Domain\Services\PackageService::class);
-$container->bind(\ZnTool\Package\Domain\Repositories\File\GroupRepository::class, function () {
-    $fileName = ! empty($_ENV['PACKAGE_GROUP_CONFIG']) ? __DIR__ . '/../../../../' . $_ENV['PACKAGE_GROUP_CONFIG'] : __DIR__ . '/../src/Domain/Data/package_group.php';
-    $repo = new \ZnTool\Package\Domain\Repositories\File\GroupRepository($fileName);
-    return $repo;
-});
-$container->bind(\ZnTool\Package\Domain\Interfaces\Repositories\PackageRepositoryInterface::class, \ZnTool\Package\Domain\Repositories\File\PackageRepository::class);
-$container->bind(\ZnTool\Package\Domain\Interfaces\Repositories\GitRepositoryInterface::class, \ZnTool\Package\Domain\Repositories\File\GitRepository::class);*/
-
 CommandHelper::registerFromNamespaceList([
     'ZnTool\Package\Commands',
 ], $container);
