ca5be602ede368d8c7eeca1dfa94abc2253ee5a8
Vitaliy <theyamshikov@yandex.ru>
2022-03-07 19:49:29 +0600

    upd

--- src/Commands/_config/bundle.php
+++ src/Commands/_config/bundle.php
@@ -3,7 +3,7 @@
 return [
 //    new \ZnLib\Fixture\Bundle(['container', 'console']),
 //    new \ZnLib\Db\Bundle(['container', 'console']),
-//    new \ZnLib\Migration\Bundle(['container', 'console']),
+//    new \ZnDatabase\Migration\Bundle(['container', 'console']),
     new \ZnTool\Package\Bundle(['container', 'console']),
 //    new \ZnTool\Phar\Bundle(['container', 'console']),
 ];
