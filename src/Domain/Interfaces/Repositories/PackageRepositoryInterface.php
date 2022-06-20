30b95fd476db757d0f1a77a8155ff260af39ea4a
ZnDeveloper <zndev@ya.ru>
2022-06-20 21:10:36 +0600

    upd

--- src/Domain/Interfaces/Repositories/PackageRepositoryInterface.php
+++ src/Domain/Interfaces/Repositories/PackageRepositoryInterface.php
@@ -6,7 +6,7 @@ use ZnCore\Domain\Interfaces\GetEntityClassInterface;
 use ZnCore\Domain\Interfaces\ReadAllInterface;
 use ZnCore\Domain\Interfaces\Repository\ReadOneInterface;
 //use ZnCore\Domain\Interfaces\Repository\RelationConfigInterface;
-use ZnCore\Domain\Interfaces\Repository\RepositoryInterface;
+use ZnCore\Base\Libs\Repository\Interfaces\RepositoryInterface;
 
 interface PackageRepositoryInterface extends RepositoryInterface, GetEntityClassInterface, ReadAllInterface, ReadOneInterface//, RelationConfigInterface
 {
